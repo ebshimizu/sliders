f63d1de7fdb3e1865d7c2865c145f9611ca41ac7
Evan Shimizu <falindrith@gmail.com>
2017-02-14 17:50:03 -0500

    submodule

--- app/darkroom/darkroom.js
+++ app/darkroom/darkroom.js
@@ -1,16 +1,30 @@
 const comp = require('../native/build/Release/compositor')
-var img = new comp.Image('C:/Users/falindrith/Dropbox/Documents/research/sliders_project/sliders/app/native/test.png')
+
+// initializes a global compositor object to operate on
+var c = new comp.Compositor()
+
+// for testing we load up three solid color test images
+c.addLayer("red", "C:/Users/falindrith/Dropbox/Documents/research/sliders_project/sliders/app/native/debug_images/solid_red.png")
+c.addLayer("green", "C:/Users/falindrith/Dropbox/Documents/research/sliders_project/sliders/app/native/debug_images/solid_green.png")
+c.addLayer("blue", "C:/Users/falindrith/Dropbox/Documents/research/sliders_project/sliders/app/native/debug_images/solid_blue.png")
 
 // Initializes html element listeners on document load
-function addListeners() {
-	$("#load").on("click", function() {
-		loadImage()
+function init() {
+	$("#render").on("click", function() {
+		renderImage()
+	})
+
+	$('#test-range').range({
+		min: 0,
+		max: 100,
+		start: 100,
+		step: 1
 	})
 }
 
 // Test function that loads an image from the compositor module and
 // then puts it in an image tag
-function loadImage() {
-	var dat = 'data:image/png;base64,' + img.getBase64()
+function renderImage() {
+	var dat = 'data:image/png;base64,' + c.render().base64()
 	$("#test").html('<img src="' + dat + '"" />')
 }

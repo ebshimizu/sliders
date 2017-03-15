eb2b17e6bf040217a9aab5d4ebea35b881b98c15
Evan Shimizu <falindrith@gmail.com>
2017-03-15 16:43:34 -0400

    added section for samples to return

--- app/darkroom/main.js
+++ app/darkroom/main.js
@@ -9,7 +9,7 @@ let win
 function createWindow () {
   // Create the browser window.
   //win = new BrowserWindow({width: 1280, height: 720, frame: false})
-  win = new BrowserWindow({width: 1280, height: 720})
+  win = new BrowserWindow({width: 1920, height: 1080})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
